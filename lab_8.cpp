0ab1c2f8f44669f9677381708ada50f79de40ac3
Firzahussain01 <114142821+Firzahussain01@users.noreply.github.com>
2022-11-22 15:24:29 +0500

    Add files via upload

---
+++ lab_8.cpp
@@ -0,0 +1,79 @@
+#include<iostream>
+using namespace std;
+
+class Plan{
+
+private:
+    double rate;
+
+public:
+
+    void getrate()
+	{
+        rate=10;
+    }
+
+    void calculateBill(int units)
+	{
+    cout<<"Bill is "<<units*(this->rate)<<"\n";
+    }
+    
+};
+
+class DomesticPlan: public Plan
+{
+
+public:
+    void getrate()
+    {
+        this->rate=200;
+    }
+};
+
+class CommercialPlan: public Plan
+{
+
+public:
+    void getrate()
+    {
+        this->rate=180;  
+    }
+};
+
+class InstitutionalPlan: public Plan
+{
+
+public:
+    void getrate()
+    {
+        this->rate=150; 
+    }
+};
+
+
+class GetPlanFactory
+{
+	
+public:
+	DomesticPlan P1;
+    CommercialPlan P2;
+    InstitutionalPlan P3;
+
+    void getPlan()
+    {
+        P1.getrate();
+        P1.calculateBill(12);
+        
+        P2.getrate();
+        P2.calculateBill(12);
+
+        P3.getrate();
+        P3.calculateBill(12);
+    }
+};
+
+int main()
+{
+GetPlanFactory P;
+P.getPlan();
+}
